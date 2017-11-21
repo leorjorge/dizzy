906c34b58e085013c7cc4783b32dfad25b2798d5
leorjorge <leonardorejorge@gmail.com>
2017-11-21 10:47:43 +0100

    Allow distance matrices to be used instead of phylogenies
    Also, some style uniformization was done, and the calculation of the Simpson diversity of hosts was removed, as it is not necessary or a very useful information.

--- R/Utility.R
+++ R/Utility.R
@@ -1,20 +1,20 @@
 #### MaxGen Functions, used to calculate the maximum possible MPD for a given number of individuals in the phylogeny.####
-#The function uses as input a phylo object and the interaction data. It should be used within the DSI calculation function and measures the maximum values of MPDs for the consumers in a given Interaction array. This function depends on the other functions below, that implement the simulated annealing optimization
+#The function uses as input a distance matrix and the interaction data. It should be used within the DSI calculation function and measures the maximum values of MPDs for a given list of consumers in a given Interaction array. This function depends on the other functions below, that implement the simulated annealing optimization
 #It returns the Maximum MPD for each herbivore species
 
-MaxGenReg <- function (Phy, Int, Spps){
-  MaxMPDReg <- vector(mode="numeric", length=length(Spps))
+MaxGenReg <- function(Phy, Int, Spps){
+  MaxMPDReg <- vector(mode = "numeric", length = length(Spps))
   Samp <- apply(Int, 1, sum)
   ConsLoc <- apply(Int, c(1,3), sum)
-  ConsLoc <- ConsLoc>0
+  ConsLoc <- ConsLoc > 0
   ResLoc <- apply(Int,c(2,3),sum)
   for(i in 1:length(MaxMPDReg)) {
-    #   print(paste("Max DSI:", Spps[i], sep = ""))
     if (Samp[Spps[i]]<2){
       MaxMPDReg[i] <- NA
     } else {
       ResSums <- rowSums(as.matrix(ResLoc[,ConsLoc[Spps[i],]]))
-      Phy.sp <- picante::prune.sample(t(ResSums[ResSums>0]), Phy)
+      Phy.sp <- Phy[rownames(Phy) %in% names(ResSums[ResSums > 0]), 
+                    colnames(Phy) %in% names(ResSums[ResSums > 0])]
       MaxMPDReg[i] <- MaxMPD(Samp[Spps[i]],Phy.sp)$value*-1
     }
   }
@@ -22,16 +22,17 @@ MaxGenReg <- function (Phy, Int, Spps){
 }
 
 
-MaxGenLoc <- function (Phy, Int, SpLocs){
-  MaxMPDLoc <- vector(mode="numeric", length=nrow(SpLocs))
+MaxGenLoc <- function(Phy, Int, SpLocs){
+  MaxMPDLoc <- vector(mode = "numeric", length = nrow(SpLocs))
   Samp <- apply(Int, c(1,3), sum)
   Abund <- apply(Int, c(2,3), sum)
-  for(i in 1:length(MaxMPDLoc)) {
-    if (Samp[SpLocs[i]]<2){
+  for (i in 1:length(MaxMPDLoc)) {
+    if (Samp[SpLocs[i]] < 2) {
       MaxMPDLoc[i] <- NA
     } else {
       LocAbund <- Abund[,SpLocs[i,2]]
-      Phy.sp <- picante::prune.sample(t(LocAbund[LocAbund>0]), Phy)
+      Phy.sp <- Phy[rownames(Phy) %in% names(LocAbund[LocAbund > 0]), 
+                    colnames(Phy) %in% names(LocAbund[LocAbund > 0])]
       MaxMPDLoc[i] <- MaxMPD(Samp[SpLocs[i,1],SpLocs[i,2]],Phy.sp)$value*-1
     }
   }
@@ -41,9 +42,8 @@ MaxGenLoc <- function (Phy, Int, SpLocs){
 ### The MPD maximization function, employing simulated annealing. It is called by both MaxGenReg and MaxGenLoc, and depends on genAbund, which is the transition function between different distributions of resource items in the phylogeny, and MPD, which is the function that calculates MPD among these individuals. This very modular design is demanded by the simulated annealing implementation in the optim function.
 
 MaxMPD <- function(N, Phylo){
-  Prob <- 1/(2^adephylo::distRoot(Phylo,method="nNode"))
-  Start <- t(table(factor(sample(Phylo$tip.label,prob=Prob,size = N,replace = T),levels=Phylo$tip.label)))
-  res <- optim(par = Start, fn = MPD, gr = genAbund, Dist = cophenetic(Phylo),
+  Start <- t(table(factor(sample(rownames(Phylo), size = N, replace = T), levels = rownames(Phylo))))
+  res <- optim(par = Start, fn = MPD, gr = genAbund, Dist = Phylo,
                method = "SANN", control = list(maxit = 10000, temp = 200, trace = 0))
   return(res)
 }
@@ -53,8 +53,8 @@ genAbund <- function(Start, Dist) {
   idx <- 1:length(Start)
   LossAbund <- sample(x = idx[Start > 0], size = 1, replace = FALSE)
   GainAbund <- sample(x = idx[-LossAbund], size = 1, replace = FALSE)
-  Start[LossAbund] <- Start[LossAbund]-1
-  Start[GainAbund] <- Start[GainAbund]+1
+  Start[LossAbund] <- Start[LossAbund] - 1
+  Start[GainAbund] <- Start[GainAbund] + 1
   Start
 }
 
