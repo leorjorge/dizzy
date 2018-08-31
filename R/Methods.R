7adb234b1b4cf93cc0f56b659e4cfff19a9a4a28
leorjorge <leonardorejorge@gmail.com>
2018-08-31 16:20:56 -0300

    make method functions consistent with the generics

--- R/Methods.R
+++ R/Methods.R
@@ -1,13 +1,13 @@
 #' @export
 #' 
 
-print.dsi <- function(object){
-  ncon <- length(object$consumers)
-  nres <- length(object$resources)
-  sing <- sum(object$samp == 1)
+print.dsi <- function(x, ...){
+  ncon <- length(x$consumers)
+  nres <- length(x$resources)
+  sing <- sum(x$samp == 1)
   cat(paste("Object of class dsi, with the distance-based specialization index calculated for", 
             ncon, "consumer species, \n using", nres, "resource types \n"))
-  print(object$DSIstar)
+  print(x$DSIstar)
   if (sing > 0) {
     cat(paste("\n",sing, " consumer species are singletons and specialization was not calculated for them"))
   }
@@ -16,7 +16,7 @@ print.dsi <- function(object){
 #' @export
 #' 
 
-summary.dsi <- function(object){
+summary.dsi <- function(object, ...){
   Res <- data.frame(N = object$samp, 
                     S = object$richness, 
                     MPD = object$MPD,
@@ -24,23 +24,23 @@ summary.dsi <- function(object){
                     DSIstar = object$DSIstar,
                     class = object$class,
                     row.names = object$consumers)
-  print(Res)
+  Res
 }
 
 #' @export
 #' 
 
-print.dsicom <- function(object){
-  ncon <- length(object$consumers)
-  nres <- length(object$resources)
-  ncom <- length(object$communities)
+print.dsicom <- function(x, ...){
+  ncon <- length(x$consumers)
+  nres <- length(x$resources)
+  ncom <- length(x$communities)
   cat(paste("Object of class dsicom, with the distance-based specialization index calculated for", 
             ncon, " consumer species, \n using", nres, " resource types in", ncom, " communities"))
   cat(paste("\n DSICom values calculated for", ncom, " communities: \n "))
-  print(object$dsicom)
-  if (is.data.frame(object$part)) {
+  print(x$dsicom)
+  if (is.data.frame(x$part)) {
     cat("\n Partition of the variability in DSI* measured locally: \n ")
-    print(object$part)
+    print(x$part)
   }
   cat("\n For individual DSI* values measured locally, access the DSIstar element directly")
 }
